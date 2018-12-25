1c15abc167a7b22911ac3109afaf77879d55c5db
Ali-Riza <aliriza.bagci@outlook.de>
2018-12-25 23:57:20 +0100

    PongGame boarder and background is finished

--- pingpong.py
+++ pingpong.py
@@ -7,24 +7,27 @@ from playsound import playsound
 import winsound
 import pygame
 import random
+import tkinter
+import time
+import turtle as tr
 
 Backgroundmusic = ['C:\PythonScripts\pingpong\Audios\A.mp3', 'C:\PythonScripts\pingpong\Audios\B.mp3', 'C:\PythonScripts\pingpong\Audios\C.mp3' , 'C:\PythonScripts\pingpong\Audios\D.mp3']
-print(random.choice(Backgroundmusic))
 
-amul = turtle.Turtle()
+
 wn = turtle.Screen()
 wn.title("Spiel: Pingpong")
-wn.setup(width=600, height=300)
 wn.tracer(0)
 wn.bgcolor("#484848")
 playsound("C:\PythonScripts\HorrorStimme.wav")
 wn.setup(width=1.0, height=1.0, startx=None, starty=None)
+wn.bgpic("C:\PythonScripts\pingpong\B.gif")
+	
 
 pygame.init()
 pygame.mixer.music.load(random.choice(Backgroundmusic))
 pygame.mixer.music.set_endevent(pygame.constants.USEREVENT)
 pygame.mixer.music.play()
- 
+
 # Zufällige Backgroundmusik
 
 
@@ -57,15 +60,47 @@ ball.shape("circle")
 ball.color("red")
 ball.penup()
 ball.goto(0, 0)
-ball.dx = 10
-ball.dy = -10
+ball.dx = 20
+ball.dy = -20
 
-#boarder
+#boarder 1
 boarder = turtle.Turtle()
 boarder.speed(0)
 boarder.shape("square")
 boarder.color("white")
-boarder.shapesize(stretch_wid=28, stretch_len= 1)
+boarder.shapesize(stretch_wid=29, stretch_len= 0.4)
+
+#boarder 3
+boarder = turtle.Turtle()
+boarder.speed(0)
+boarder.shape("square")
+boarder.goto(400, 0)
+boarder.color("white")
+boarder.shapesize(stretch_wid=29, stretch_len= 0.4)
+
+#boarder 3
+boarder = turtle.Turtle()
+boarder.speed(0)
+boarder.shape("square")
+boarder.goto(-400, 0)
+boarder.color("white")
+boarder.shapesize(stretch_wid=29, stretch_len= 0.4)
+
+#boarder 4
+boarder = turtle.Turtle()
+boarder.speed(0)
+boarder.shape("square")
+boarder.goto(1, 295)
+boarder.color("white")
+boarder.shapesize(stretch_wid=0.4, stretch_len= 40)
+
+#boarder 4
+boarder = turtle.Turtle()
+boarder.speed(0)
+boarder.shape("square")
+boarder.goto(-1, -295)
+boarder.color("white")
+boarder.shapesize(stretch_wid=0.4, stretch_len= 40)
 
 # Pen
 pen = turtle.Turtle()
@@ -73,8 +108,8 @@ pen.speed(0)
 pen.color("yellow")
 pen.penup()
 pen.hideturtle()
-pen.goto(0, 260)
-pen.write("0      0", align="center", font=("Courier", 50, "bold"))
+pen.goto(0, 300)
+pen.write("0            0", align="center", font=("Bahnschrift Light", 50, "bold"))
 
 # Function
 def paddle_a_up():
@@ -128,7 +163,7 @@ while True:
         ball.dx *= -1
         score_a += 1
         pen.clear()
-        pen.write("{}      {}".format(score_a, score_b), align="center", font=("Courier", 50, "bold"))
+        pen.write("{}            {}".format(score_a, score_b), align="center", font=("Bahnschrift Light", 50, "bold"))
 		
 		
     if  ball.xcor() < -390:
@@ -136,7 +171,7 @@ while True:
         ball.dx *= -1
         score_b += 1
         pen.clear()
-        pen.write("{}      {}".format(score_a, score_b), align="center", font=("Courier", 50, "bold"))
+        pen.write("{}            {}".format(score_a, score_b), align="center", font=("Bahnschrift Light", 50, "bold"))
 
 
     # Paddle and ball collisions
@@ -150,9 +185,32 @@ while True:
         ball.dx *= -1
         playsound("C:\PythonScripts\pingpong\Audios\Gun+Silencer.mp3")
 		
-    if  score_a == 20:
-	    pen.write("Spieler 1 hat gewonnen ", align="center", font=("Courier", 50, "bold"))
+    if  score_a == 15:
+	    pen.write("Spieler 2 hat gewonnen ", align="left", font=("Courier", 18, "bold"))
 	    
-		  
-    if  score_b == 20:
-	    pen.write("Spieler 2 hat gewonnen ", align="center", font=("Courier", 50, "bold"))
+        
+    if  score_b == 45:
+        pen.write("Spieler 2 hat gewonnen ", align="right", font=("Courier", 20, "bold"))
+		
+	# Boarder checking padlles
+    if  paddle_a.ycor() < -295:
+        paddle_a.sety(-250)
+		
+    if  paddle_a.ycor() > 295:
+        paddle_a.sety(250)
+		
+    if  paddle_b.ycor() < -295:
+        paddle_b.sety(-250)
+		
+    if  paddle_b.ycor() > 295:
+        paddle_b.sety(250)
+		
+		
+		
+		
+
+	
+	
+
+
+
